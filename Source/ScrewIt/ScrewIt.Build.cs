2b11f78ec1e51ee0f676d72db3f75d6c8214e518
Angel Pons <th3fanbus@gmail.com>
2024-10-16 10:55:33 +0200

    ScrewIt version 1.1.0
    Update to FG 1.0
    
    Signed-off-by: Angel Pons <th3fanbus@gmail.com>

--- Source/ScrewIt/ScrewIt.Build.cs
+++ Source/ScrewIt/ScrewIt.Build.cs
@@ -1,22 +1,24 @@
 /* SPDX-License-Identifier: MPL-2.0 */
 
 using UnrealBuildTool;
-using System.IO;
-using System;
 
 public class ScrewIt : ModuleRules
 {
     public ScrewIt(ReadOnlyTargetRules Target) : base(Target)
     {
+        DefaultBuildSettings = BuildSettingsVersion.Latest;
+        ShadowVariableWarningLevel = WarningLevel.Error;
         PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+        bLegacyParentIncludePaths = false;
+        CppStandard = CppStandardVersion.Cpp20;
         bUseUnity = false;
 
         PublicDependencyModuleNames.AddRange(new string[] {
-            "Core", "CoreUObject",
-            "Engine",
-            "InputCore",
-            "SlateCore", "Slate", "UMG",
-            "DummyHeaders", "FactoryGame", "SML"
+            "Core", "CoreUObject", "Engine",
+            "DeveloperSettings", "PhysicsCore", "InputCore",
+            "AssetRegistry", "RenderCore", "RHI",
+            "SlateCore", "Slate", "UMG", "GameplayTags",
+            "DummyHeaders", "FactoryGame", "SML",
         });
     }
 }
